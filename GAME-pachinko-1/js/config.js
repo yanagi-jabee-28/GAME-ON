9b2fdce41f1f6071cc5f41250bd10d36ede4f860
yanagi-jabee_28 <kaitomanauto.1130@gmail.com>
2025-08-22 20:13:51 +0900

    fix: GAME_CONFIGのbaseHeightを675に変更し、ゲームエリアの描画に影響を与える設定を修正しました

--- GAME-pachinko-1/js/config.js
+++ GAME-pachinko-1/js/config.js
@@ -19,7 +19,7 @@ const GAME_CONFIG = {
 	// デザイン時の基準幅。width を変更した際、この値との差分で水平オフセットを計算します。
 	baseWidth: 450,
 	// デザイン時の基準高さ。height を変更した際、この値との差分で垂直オフセットを計算します。
-	baseHeight: 600,
+	baseHeight: 675,
 	height: 900, // ゲームエリアの描画高 (px)
 	renderOptions: {
 		wireframes: false, // falseにするとオブジェクトが塗りつぶされる (trueだと線画)
