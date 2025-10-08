cd3e4d51068f3ad6df0613e1d96e6efdba4b2750
shiguri <shiguri01.gm@gmail.com>
2025-10-08 11:13:07 +0900

    feat: ViteにTailwind CSSをセットアップ

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	plugins: [tailwindcss()],
 	build: {
 		rollupOptions: {
 			input: {
