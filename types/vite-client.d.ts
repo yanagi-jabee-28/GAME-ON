179132ce81af4faa38f02fbda41885a7af3c300a
yanagi-jabee_28 <kaitomanauto.1130@gmail.com>
2025-10-17 17:03:18 +0900

    refactor: 不要な型定義ファイルを削除し、tsconfigを整理

--- types/vite-client.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "vite/client" {
-	interface ImportMetaEnv {
-		readonly [key: string]: string | boolean | undefined;
-	}
-
-	interface ImportMeta {
-		readonly env: ImportMetaEnv;
-	}
-}
--- types/vite/index.d.ts
+++
@@ -1,7 +0,0 @@
-// Vite module declarations
-declare module "vite" {
-	export * from "vite/client";
-}
-
-// Re-export client types
-export * from "./client";
